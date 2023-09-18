1e1963cb0e6925c7f64b3d23b1cff00605b861d5
Jascha-Biederstedt <j.biederstedt@gmx.de>
2023-09-18 14:53:34 +0200

    Add navigation to homepage

--- app/page.js
+++ app/page.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import Link from 'next/link';
 
 const HomePage = () => {
   return (
     <div>
       <h1>HomePage</h1>
       <button className='btn btn-primary'>Primary</button>
+      <ul>
+        <li>
+          <Link href={'/'}>Home</Link>
+        </li>
+        <li>
+          <Link href={'/about'}>About</Link>
+        </li>
+      </ul>
     </div>
   );
 };
