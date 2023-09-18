26396ca4e2e5976b8a8f7f5d168cc866bfbd0f25
Jascha-Biederstedt <j.biederstedt@gmx.de>
2023-09-18 14:59:47 +0200

    Add phone number to contact page

--- app/about/contact/page.jsx
+++ app/about/contact/page.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>Phone: 555-555-5555</p>
+    </div>
+  );
 };
 
 export default Contact;
