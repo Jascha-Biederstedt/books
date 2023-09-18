9cd76ff1eb0eff91b8863894f3d63eaec68a4f22
Jascha-Biederstedt <j.biederstedt@gmx.de>
2023-09-18 15:47:42 +0200

    Implement fetching github user data

--- app/githubusers/page.jsx
+++ app/githubusers/page.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 
-const GitHubUsers = () => {
+const fetchGitHubUsers = async () => {
+  const res = await fetch('https://api.github.com/search/users?q=greg');
+  const data = await res.json();
+  return data.items;
+};
+
+const GitHubUsers = async () => {
+  const users = await fetchGitHubUsers();
+
+  console.log(users);
+
   return (
     <div>
       <h1>GitHub Users</h1>
