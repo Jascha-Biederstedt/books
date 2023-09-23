2b8105e5f9b633ae30edb2ae0b45c6e112d19846
Jascha-Biederstedt <j.biederstedt@gmx.de>
2023-09-23 14:24:29 +0200

    Style the homepage

--- components/Books.jsx
+++ components/Books.jsx
@@ -12,27 +12,29 @@ const Books = async () => {
 
   return (
     <div>
-      <h1>Books</h1>
-      {books.map(book => (
-        <div key={book.id}>
-          <div className='card w-96 bg-base-100 shadow-xl'>
-            <figure>
-              <img src={book.img} width='200' height='150' alt='book cover' />
-            </figure>
-            <div className='card-body'>
-              <h2 className='card-title'>{book.id}</h2>
-              <p>{book.title}</p>
-              <div className='card-actions justify-end'>
-                <Link href={book.link} className='btn btn-primary'>
-                  See in Amazon
-                </Link>
-                <button className='btn btn-error'>Delete</button>
+      <h1 className='text-center pt-8 pb-10 text-lg font-bold'>Books</h1>
+      <div className='flex flex-wrap justify-center'>
+        {books.map(book => (
+          <div key={book.id} className='mx-4 mb-4'>
+            <div className='card w-96 bg-base-100 shadow-xl'>
+              <figure>
+                <img src={book.img} width='200' height='150' alt='book cover' />
+              </figure>
+              <div className='card-body'>
+                <h2 className='card-title'>{book.id}</h2>
+                <p>{book.title}</p>
+                <div className='card-actions justify-end'>
+                  <Link href={book.link} className='btn btn-primary'>
+                    See in Amazon
+                  </Link>
+                  <button className='btn btn-error'>Delete</button>
+                </div>
               </div>
             </div>
+            <br />
           </div>
-          <br />
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
