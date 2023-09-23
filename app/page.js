0da3f5afa5b92063e88fc4450b95f4ae6537267d
Jascha-Biederstedt <j.biederstedt@gmx.de>
2023-09-23 13:55:58 +0200

    Implement showing book cards on homepage

--- app/page.js
+++ app/page.js
@@ -1,23 +1,12 @@
 import React from 'react';
-import Link from 'next/link';
+
+import Books from '@/components/Books';
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>HomePage</h1>
-      <button className='btn btn-primary'>Primary</button>
-      <ul>
-        <li>
-          <Link href='/'>Home</Link>
-        </li>
-        <li>
-          <Link href='/about'>About</Link>
-        </li>
-        <li>
-          <Link href='/about/contact'>Contact</Link>
-        </li>
-      </ul>
-    </div>
+    <>
+      <Books />
+    </>
   );
 };
 
