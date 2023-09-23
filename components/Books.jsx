467a7aee79a5f3e8519cc7da1707e3f868d8b399
Jascha-Biederstedt <j.biederstedt@gmx.de>
2023-09-23 15:46:55 +0200

    Add form for searching books

--- components/Books.jsx
+++ components/Books.jsx
@@ -13,6 +13,7 @@ const getBooks = async () => {
 const Books = () => {
   const [books, setBooks] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState('');
 
   useEffect(() => {
     getBooks().then(books => {
@@ -23,9 +24,29 @@ const Books = () => {
 
   if (isLoading) return <LoadingPage />;
 
+  const handleSubmit = async event => {
+    event.preventDefault();
+
+    console.log(query);
+  };
+
   return (
     <div>
       <h1 className='text-center pt-8 pb-10 text-lg font-bold'>Books</h1>
+      <form onSubmit={handleSubmit} className='text-center mb-16'>
+        <input
+          type='text'
+          placeholder='Search Books...'
+          value={query}
+          onChange={event => {
+            setQuery(event.target.value);
+          }}
+          className='input input-bordered w-full max-w-xs mr-3'
+        />
+        <button type='submit' className='btn btn-primary'>
+          Search
+        </button>
+      </form>
       <div className='flex flex-wrap justify-center'>
         {books.map(book => (
           <div key={book.id} className='mx-4 mb-4 text-center'>
