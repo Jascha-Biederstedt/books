7dd60b824c97432ff7b0cf91866c87ccfbc7bd15
Jascha-Biederstedt <j.biederstedt@gmx.de>
2023-09-23 15:05:57 +0200

    Implement filtering books by search query

--- app/api/books/search/route.js
+++ app/api/books/search/route.js
@@ -4,8 +4,11 @@ import books from '../data.json';
 
 export const GET = async req => {
   const { searchParams } = new URL(req.url);
+  const query = searchParams.get('query');
 
-  console.log(searchParams.get('query'));
+  const filteredBooks = books.filter(book => {
+    return book.title.toLowerCase().includes(query.toLowerCase());
+  });
 
-  return NextResponse.json(books);
+  return NextResponse.json(filteredBooks);
 };
