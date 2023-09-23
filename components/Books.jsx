a8c0b43b1e869f47ac0d5e75b862cff0ce42c2c0
Jascha-Biederstedt <j.biederstedt@gmx.de>
2023-09-23 13:54:31 +0200

    Implement rendering books in Books component

--- components/Books.jsx
+++ components/Books.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 
 const getBooks = async () => {
   const res = await fetch('http://localhost:3000/api/books');
@@ -9,7 +10,31 @@ const getBooks = async () => {
 const Books = async () => {
   const books = await getBooks();
 
-  return <div>Books</div>;
+  return (
+    <div>
+      <h1>Books</h1>
+      {books.map(book => (
+        <div key={book.id}>
+          <div className='card w-96 bg-base-100 shadow-xl'>
+            <figure>
+              <img src={book.img} width='200' height='150' alt='book cover' />
+            </figure>
+            <div className='card-body'>
+              <h2 className='card-title'>{book.id}</h2>
+              <p>{book.title}</p>
+              <div className='card-actions justify-end'>
+                <Link href={book.link} className='btn btn-primary'>
+                  See in Amazon
+                </Link>
+                <button className='btn btn-error'>Delete</button>
+              </div>
+            </div>
+          </div>
+          <br />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Books;
