314118b7b00e4ed463680bf53065ce94bc7fae0c
Jascha-Biederstedt <j.biederstedt@gmx.de>
2023-09-23 15:11:47 +0200

    Implement adding new book in books route

--- app/api/books/route.js
+++ app/api/books/route.js
@@ -1,6 +1,22 @@
-import books from './data.json';
 import { NextResponse } from 'next/server';
+
+import books from './data.json';
 
 export const GET = async req => {
   return NextResponse.json(books);
+};
+
+export const POST = async req => {
+  const { title, link, img } = await req.json();
+
+  const newBook = {
+    id: books.length + 1,
+    title,
+    link,
+    img,
+  };
+
+  books.push(newBook);
+
+  return NextResponse.json('Book added successfully');
 };
