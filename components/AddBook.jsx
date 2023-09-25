5268f702eec16af2f3647b4f249addfb50d5e2d6
Jascha-Biederstedt <j.biederstedt@gmx.de>
2023-09-25 15:21:21 +0200

    Style the add book modal

--- components/AddBook.jsx
+++ components/AddBook.jsx
@@ -22,11 +22,11 @@ const AddBook = () => {
               ✕
             </button>
           </form>
-          <h3 className='font-bold text-lg'>Add New Book</h3>
+          <h3 className='font-bold text-lg mb-4'>Add New Book</h3>
           <input
             type='text'
             placeholder='Enter New Book Title'
-            className='input input-bordered w-full max-w-xs'
+            className='input input-bordered w-full max-w-xs mr-4'
           />
           <button type='submit' className='btn btn-primary'>
             Add Book
