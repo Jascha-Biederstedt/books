051425cc73e2bddb8a3073f71528be0153ea3177
Jascha-Biederstedt <j.biederstedt@gmx.de>
2023-09-25 15:34:07 +0200

    Add handleSubmitNewBook fn

--- components/AddBook.jsx
+++ components/AddBook.jsx
@@ -3,6 +3,14 @@ import React, { useState } from 'react';
 
 const AddBook = () => {
   const [modalOpen, setModalOpen] = useState(false);
+  const [newBookTitle, setNewBookTitle] = useState('');
+
+  const handleSubmitNewBook = event => {
+    event.preventDefault();
+
+    console.log(newBookTitle);
+    setNewBookTitle('');
+  };
 
   return (
     <div className='text-center mb-12'>
@@ -14,23 +22,25 @@ const AddBook = () => {
         className={`modal ${modalOpen ? 'modal-open' : ''}`}
       >
         <div className='modal-box'>
-          <form method='dialog'>
+          <form method='dialog' onSubmit={handleSubmitNewBook}>
             <button
               onClick={() => setModalOpen(false)}
               className='btn btn-sm btn-circle btn-ghost absolute right-2 top-2'
             >
               ✕
             </button>
+            <h3 className='font-bold text-lg mb-4'>Add New Book</h3>
+            <input
+              type='text'
+              placeholder='Enter New Book Title'
+              className='input input-bordered w-full max-w-xs mr-3'
+              value={newBookTitle}
+              onChange={event => setNewBookTitle(event.target.value)}
+            />
+            <button type='submit' className='btn btn-primary'>
+              Add Book
+            </button>
           </form>
-          <h3 className='font-bold text-lg mb-4'>Add New Book</h3>
-          <input
-            type='text'
-            placeholder='Enter New Book Title'
-            className='input input-bordered w-full max-w-xs mr-4'
-          />
-          <button type='submit' className='btn btn-primary'>
-            Add Book
-          </button>
         </div>
       </dialog>
     </div>
