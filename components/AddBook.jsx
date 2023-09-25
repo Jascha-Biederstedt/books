db3fda3ad1362192afe138ffdda1a230e286af0f
Jascha-Biederstedt <j.biederstedt@gmx.de>
2023-09-25 15:18:38 +0200

    Add input field to add book modal

--- components/AddBook.jsx
+++ components/AddBook.jsx
@@ -23,6 +23,11 @@ const AddBook = () => {
             </button>
           </form>
           <h3 className='font-bold text-lg'>Add New Book</h3>
+          <input
+            type='text'
+            placeholder='Enter New Book Title'
+            className='input input-bordered w-full max-w-xs'
+          />
         </div>
       </dialog>
     </div>
