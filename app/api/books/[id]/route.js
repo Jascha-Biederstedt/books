dcc926d1b5ff0d93c673698297c069432efe5817
Jascha-Biederstedt <j.biederstedt@gmx.de>
2023-09-27 18:36:51 +0200

    Implement deleting book in backend

--- app/api/books/[id]/route.js
+++ app/api/books/[id]/route.js
@@ -5,7 +5,9 @@ import books from '@/app/api/books/data.json';
 export const DELETE = async (request, { params }) => {
   const id = params.id;
 
-  console.log('id', id);
+  const index = books.findIndex(book => book.id === id);
+
+  if (index !== -1) books.splice(index, 1);
 
   return new NextResponse({ 'Book deleted': id });
 };
