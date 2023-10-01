cdbcb094611d6ea3e83aa1bec5614f78423056e4
Jascha-Biederstedt <j.biederstedt@gmx.de>
2023-10-01 15:49:16 +0200

    Implement searching for books with prisma

--- app/api/books/search/route.js
+++ app/api/books/search/route.js
@@ -1,13 +1,13 @@
 import { NextResponse } from 'next/server';
 
-import books from '../data.json';
+import prisma from '../../../db';
 
 export const GET = async req => {
   const { searchParams } = new URL(req.url);
   const query = searchParams.get('query');
 
-  const filteredBooks = books.filter(book => {
-    return book.title.toLowerCase().includes(query.toLowerCase());
+  const filteredBooks = await prisma.book.findMany({
+    where: { title: { contains: query } },
   });
 
   return NextResponse.json(filteredBooks);
