1f0e142f4abf796f64d5a73c42642901acbc0ae2
Jascha-Biederstedt <j.biederstedt@gmx.de>
2023-10-02 15:48:03 +0200

    Style the contact page heading

--- app/about/contact/page.jsx
+++ app/about/contact/page.jsx
@@ -3,8 +3,7 @@ import React from 'react';
 const Contact = () => {
   return (
     <div>
-      <h1>Contact</h1>
-      <p>Phone: 555-555-5555</p>
+      <h1 className='px-8 py-4 font-bold'>Contact</h1>
     </div>
   );
 };
